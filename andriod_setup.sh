c3e926fc6d2d56509ae71e4f4124ca4b5fae6b17
Deliander <72639023+Deliander@users.noreply.github.com>
2022-07-03 13:51:52 +1000

    Update andriod_setup.sh

--- andriod_setup.sh
+++ andriod_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "update sys"
-apt update >> /dev/null
+apk update >> /dev/null
 clear
 sleep 2
 echo "install figlet"
-apt install -y figlet
+apk add figlet
 clear
 sleep 03
 figlet update INSTA-BF
@@ -13,15 +13,15 @@ chmod +x *
 clear
 sleep 02
 figlet install python
-apt install -y python >> /dev/null
+apk add python >> /dev/null
 clear
 sleep 02
 figlet install python 2
-apt install -y python2 >> /dev/null
+apk add python2 >> /dev/null
 clear
 sleep 02
 figlet install python 3
-apt install -y python3 >> /dev/null
+apk add python3 >> /dev/null
 clear
 sleep 02
 figlet installed
